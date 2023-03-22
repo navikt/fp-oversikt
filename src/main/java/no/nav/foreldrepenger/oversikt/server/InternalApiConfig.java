1105a938e5a8e3a2d4bf320bb035eeda78d7e3c0
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-03-22 09:14:02 +0100

    Server med enkel resttjenest (#1)
    ---------
    
    Co-authored-by: Steffen Pøhner <steffen.h.pohner@nav.no>
    Co-authored-by: Anders Palfi <anders.palfi@nav.no>

---
+++ src/main/java/no/nav/foreldrepenger/oversikt/server/InternalApiConfig.java
@@ -0,0 +1,20 @@
+package no.nav.foreldrepenger.oversikt.server;
+
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("internal")
+public class InternalApiConfig extends Application {
+
+    public InternalApiConfig() {
+        // CDI
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Set.of(Rest.class);
+    }
+
+}
