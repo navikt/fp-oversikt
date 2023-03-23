91e8f553818c45f1d17cf2998cf13562fb77ad8c
Anders Palfi <anders.palfi@nav.no>
2023-03-23 10:08:45 +0100

    subject cleanup

---
+++ src/main/java/no/nav/foreldrepenger/oversikt/server/SubjectHandlerCleanupRequestListener.java
@@ -0,0 +1,26 @@
+package no.nav.foreldrepenger.oversikt.server;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.vedtak.sikkerhet.context.ContextCleaner;
+
+@WebListener
+public class SubjectHandlerCleanupRequestListener implements ServletRequestListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SubjectHandlerCleanupRequestListener.class);
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+
+        try {
+            ContextCleaner.enusureCleanContext();
+        } catch (Exception e) {
+            LOG.info("Kunne ikke fjerne subject", e);
+        }
+    }
+}
