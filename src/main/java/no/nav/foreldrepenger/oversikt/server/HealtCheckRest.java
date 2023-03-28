d4c4ba302564fb9966296601ecc3b357d5228e0a
Anders Palfi <anders.palfi@nav.no>
2023-03-28 13:33:07 +0200

    db i dev

--- src/main/java/no/nav/foreldrepenger/oversikt/server/HealtCheckRest.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/server/HealtCheckRest.java
@@ -3,45 +3,38 @@ package no.nav.foreldrepenger.oversikt.server;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import no.nav.foreldrepenger.konfig.Environment;
+
 @Path("/health")
 @ApplicationScoped
 public class HealtCheckRest {
 
-    private static final CacheControl CC = cacheControl();
     private static final Logger LOG = LoggerFactory.getLogger(HealtCheckRest.class);
+    public static final Environment ENV = Environment.current();
 
     @GET
     @Path("/isAlive")
     public Response isAlive() {
-        LOG.info("isAlive kalt");
-        return Response.ok().cacheControl(CC).build();
+        LOG.info("isAlive kalt {}", ENV.getProperty("NAIS_DATABASE_FPOVERSIKT_FPOVERSIKT_DATABASE", "mangler"));
+        return Response.ok().build();
     }
 
     @GET
     @Path("/isReady")
     public Response isReady() {
         LOG.info("isReady kalt");
-        return Response.ok().cacheControl(CC).build();
+        return Response.ok().build();
     }
 
     @GET
     @Path("/preStop")
     public Response preStop() {
         LOG.info("preStop kalt");
-        return Response.ok().cacheControl(CC).build();
-    }
-
-    private static CacheControl cacheControl() {
-        var cc = new CacheControl();
-        cc.setNoCache(true);
-        cc.setNoStore(true);
-        cc.setMustRevalidate(true);
-        return cc;
+        return Response.ok().build();
     }
 }
