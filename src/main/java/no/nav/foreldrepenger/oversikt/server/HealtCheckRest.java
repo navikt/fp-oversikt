fd9b7b83a0a3e5befd22e8654da5c725e7d2f25d
Anders Palfi <anders.palfi@nav.no>
2023-03-28 15:44:31 +0200

    Mer db oppsett

--- src/main/java/no/nav/foreldrepenger/oversikt/server/HealtCheckRest.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/server/HealtCheckRest.java
@@ -8,19 +8,16 @@ import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.foreldrepenger.konfig.Environment;
-
 @Path("/health")
 @ApplicationScoped
 public class HealtCheckRest {
 
     private static final Logger LOG = LoggerFactory.getLogger(HealtCheckRest.class);
-    public static final Environment ENV = Environment.current();
 
     @GET
     @Path("/isAlive")
     public Response isAlive() {
-        LOG.info("isAlive kalt {}", ENV.getProperty("NAIS_DATABASE_FPOVERSIKT_FPOVERSIKT_DATABASE", "mangler"));
+        LOG.info("isAlive kalt {}");
         return Response.ok().build();
     }
 
