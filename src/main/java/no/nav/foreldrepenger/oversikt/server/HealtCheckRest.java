f96fa0ec5d6498dd1dace21ff2bfffe7e2cf4af1
espenwaaga <espen.waaga@nav.no>
2023-04-18 13:14:13 +0200

    Fjerner logging av isAlive/isReady/preStop

--- src/main/java/no/nav/foreldrepenger/oversikt/server/HealtCheckRest.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/server/HealtCheckRest.java
@@ -5,33 +5,25 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Path("/health")
 @ApplicationScoped
 public class HealtCheckRest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(HealtCheckRest.class);
-
     @GET
     @Path("/isAlive")
     public Response isAlive() {
-        LOG.info("isAlive kalt");
         return Response.ok().build();
     }
 
     @GET
     @Path("/isReady")
     public Response isReady() {
-        LOG.info("isReady kalt");
         return Response.ok().build();
     }
 
     @GET
     @Path("/preStop")
     public Response preStop() {
-        LOG.info("preStop kalt");
         return Response.ok().build();
     }
 }
