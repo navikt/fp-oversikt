3f03a235eba86a2d85d34bfe2720a29051eb7dc4
espenwaaga <espen.waaga@nav.no>
2023-04-19 15:33:42 +0200

    Bruker riktig url og scope for PDLklient og registere fpsak og pdl som outbound hosts

--- src/main/java/no/nav/foreldrepenger/oversikt/saker/PdlKlient.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/saker/PdlKlient.java
@@ -6,8 +6,12 @@ import no.nav.vedtak.felles.integrasjon.person.AbstractPersonKlient;
 import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
 import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
-@RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, endpointProperty = "pdl.base.url", endpointDefault = "http://pdl-api.pdl/graphql",
-    scopesProperty = "pdl.scopes", scopesDefault = "api://prod-fss.pdl.pdl-api/.default")
+@RestClientConfig(
+    tokenConfig = TokenFlow.ADAPTIVE,
+    endpointProperty = "pdl.base.url",
+    endpointDefault = "https://pdl-api.prod-fss-pub.nais.io/graphql",
+    scopesProperty = "pdl.scopes",
+    scopesDefault = "api://prod-fss.pdl.pdl-api/.default")
 @Dependent
 public class PdlKlient extends AbstractPersonKlient {
 
