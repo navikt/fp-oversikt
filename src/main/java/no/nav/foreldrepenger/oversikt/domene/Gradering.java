c99c149ad6e45518ca038564b196cc0621aafcd8
Anders Palfi <anderspalfi@gmail.com>
2023-05-30 11:21:46 +0200

    Fikser noe gradering uten arbeidsgiver (#44)

--- src/main/java/no/nav/foreldrepenger/oversikt/domene/Gradering.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/domene/Gradering.java
@@ -4,7 +4,8 @@ import no.nav.foreldrepenger.common.innsyn.Aktivitet;
 
 public record Gradering(Prosent prosent, UttakAktivitet uttakAktivitet) {
     public no.nav.foreldrepenger.common.innsyn.Gradering tilDto() {
+        var arbeidsgiver = uttakAktivitet().arbeidsgiver();
         return new no.nav.foreldrepenger.common.innsyn.Gradering(prosent().decimalValue(),
-            new Aktivitet(uttakAktivitet().type().tilDto(), uttakAktivitet().arbeidsgiver().tilDto()));
+            new Aktivitet(uttakAktivitet().type().tilDto(), arbeidsgiver == null ? null : arbeidsgiver.tilDto()));
     }
 }
