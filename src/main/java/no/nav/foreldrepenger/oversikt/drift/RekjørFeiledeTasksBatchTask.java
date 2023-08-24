cb92991c17acc43f107ee0696d1f7a8cc2d0f8a5
Anders Palfi <anders.palfi@nav.no>
2023-08-24 10:28:48 +0200

    cron fix

--- src/main/java/no/nav/foreldrepenger/oversikt/drift/RekjørFeiledeTasksBatchTask.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/drift/RekjørFeiledeTasksBatchTask.java
@@ -11,7 +11,7 @@ import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
 @ApplicationScoped
-@ProsessTask(value = "retry.feilendeTasks", cronExpression = "0 0 0-24/2 * * *", maxFailedRuns = 1)
+@ProsessTask(value = "retry.feilendeTasks", cronExpression = "0 0 0-23/2 * * *", maxFailedRuns = 1)
 public class RekjørFeiledeTasksBatchTask implements ProsessTaskHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(RekjørFeiledeTasksBatchTask.class);
