b005ee1a5681ba92e8d4aeaaf7a6135e90d0ea36
espenwaaga <espen.waaga@nav.no>
2023-09-04 15:13:03 +0200

    Fix: Oppdatere saf.base.url uten /graphql endelse

--- src/main/java/no/nav/foreldrepenger/oversikt/arkiv/SafKlient.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/arkiv/SafKlient.java
@@ -9,7 +9,7 @@ import no.nav.vedtak.felles.integrasjon.saf.AbstractSafKlient;
 @RestClientConfig(
     tokenConfig = TokenFlow.AZUREAD_CC,
     endpointProperty = "saf.base.url",
-    endpointDefault = "https://saf.prod-fss-pub.nais.io/graphql",
+    endpointDefault = "https://saf.prod-fss-pub.nais.io",
     scopesProperty = "saf.scopes",
     scopesDefault = "api://prod-fss.teamdokumenthandtering.saf/.default")
 @Dependent
