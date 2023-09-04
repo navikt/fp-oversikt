1d3cafb6f2f7a3dbb37dcad7402e0f867f0f0567
Steffen Pøhner <steffen.h.pohner@nav.no>
2023-09-04 09:21:11 +0200

    Legger til safklient (#112)

---
+++ src/main/java/no/nav/foreldrepenger/oversikt/arkiv/SafKlient.java
@@ -0,0 +1,23 @@
+package no.nav.foreldrepenger.oversikt.arkiv;
+
+
+import jakarta.enterprise.context.Dependent;
+import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
+import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
+import no.nav.vedtak.felles.integrasjon.saf.AbstractSafKlient;
+
+@RestClientConfig(
+    tokenConfig = TokenFlow.AZUREAD_CC,
+    endpointProperty = "saf.base.url",
+    endpointDefault = "https://saf.prod-fss-pub.nais.io/graphql",
+    scopesProperty = "saf.scopes",
+    scopesDefault = "api://prod-fss.teamdokumenthandtering.saf/.default")
+@Dependent
+// Sakogarkiv klient
+public class SafKlient extends AbstractSafKlient {
+
+    public SafKlient() {
+        super();
+    }
+
+}
