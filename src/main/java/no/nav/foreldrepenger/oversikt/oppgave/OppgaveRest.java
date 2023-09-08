61d1a9634bbce05c1585a13e43f935836b3bc801
espenwaaga <espen.waaga@nav.no>
2023-09-08 16:10:17 +0200

    Input validering på innkommende saksnummer

--- src/main/java/no/nav/foreldrepenger/oversikt/oppgave/OppgaveRest.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/oppgave/OppgaveRest.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.oversikt.oppgave;
 
+import static no.nav.foreldrepenger.common.domain.validation.InputValideringRegex.BARE_TALL;
 import static no.nav.foreldrepenger.oversikt.saker.TilgangsstyringBorger.sjekkAtKallErFraBorger;
 
 import java.util.List;
@@ -11,8 +12,8 @@ import org.slf4j.LoggerFactory;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -49,12 +50,12 @@ public class OppgaveRest {
     @GET
     @Path("/manglendevedlegg")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<DokumentType> manglendeVedlegg(@NotNull @QueryParam("saksnummer") @Valid Saksnummer saksnummer) {
+    public List<DokumentType> manglendeVedlegg(@NotNull @QueryParam("saksnummer") @Pattern(regexp = BARE_TALL) String saksnummer) {
         sjekkAtKallErFraBorger();
         tilgangssjekkMyndighetsalder();
         sakKobletTilAktørGuard(saksnummer);
-        LOG.info("Henter manglede vedlegg for sak {}", saksnummer.value());
-        return oppgaver.manglendeVedlegg(saksnummer);
+        LOG.info("Henter manglede vedlegg for sak {}", saksnummer);
+        return oppgaver.manglendeVedlegg(new Saksnummer(saksnummer));
     }
 
     @GET
@@ -73,9 +74,9 @@ public class OppgaveRest {
         }
     }
 
-    private void sakKobletTilAktørGuard(Saksnummer saksnummer) {
+    private void sakKobletTilAktørGuard(String saksnummer) {
         var aktørId = innloggetBruker.aktørId();
-        if (!sakRepository.erSakKobletTilAktør(saksnummer, aktørId)) {
+        if (!sakRepository.erSakKobletTilAktør(new Saksnummer(saksnummer), aktørId)) {
             throw new ManglerTilgangException(FeilKode.IKKE_TILGANG);
         }
     }
