ab8b05c81b94db7828b7e008a249e2ac38065045
espenwaaga <espen.waaga@nav.no>
2023-09-10 09:59:34 +0200

    API for oppgaver må legges inn i ApiConfig

--- src/main/java/no/nav/foreldrepenger/oversikt/server/ApiConfig.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/server/ApiConfig.java
@@ -19,6 +19,7 @@ import jakarta.ws.rs.core.Application;
 import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.foreldrepenger.oversikt.drift.ManuellOppdateringAvSakDriftTjeneste;
 import no.nav.foreldrepenger.oversikt.drift.ProsessTaskRestTjeneste;
+import no.nav.foreldrepenger.oversikt.oppgave.OppgaveRest;
 import no.nav.foreldrepenger.oversikt.saker.AnnenPartRest;
 import no.nav.foreldrepenger.oversikt.saker.SakerRest;
 import no.nav.vedtak.exception.TekniskException;
@@ -54,7 +55,7 @@ public class ApiConfig extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         // eksponert grensesnitt bak sikkerhet
-        return Set.of(SakerRest.class, AnnenPartRest.class, ProsessTaskRestTjeneste.class, ManuellOppdateringAvSakDriftTjeneste.class, OpenApiResource.class, GeneralRestExceptionMapper.class);
+        return Set.of(OppgaveRest.class, SakerRest.class, AnnenPartRest.class, ProsessTaskRestTjeneste.class, ManuellOppdateringAvSakDriftTjeneste.class, OpenApiResource.class, GeneralRestExceptionMapper.class);
     }
 
     @Override
