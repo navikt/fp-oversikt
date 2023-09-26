05336b40113740ef5416e086335665c3137a8a75
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-09-26 11:02:23 +0200

    Legger til endepunkt for å hente inntektsmeldinger på sak (#148)

---
+++ src/main/java/no/nav/foreldrepenger/oversikt/innhenting/inntektsmelding/InntektsmeldingDto.java
@@ -0,0 +1,6 @@
+package no.nav.foreldrepenger.oversikt.innhenting.inntektsmelding;
+
+import java.time.LocalDateTime;
+
+record InntektsmeldingDto(LocalDateTime opprettet) {
+}
---
+++ src/main/java/no/nav/foreldrepenger/oversikt/innhenting/inntektsmelding/InntektsmeldingRest.java
@@ -0,0 +1,47 @@
+package no.nav.foreldrepenger.oversikt.innhenting.inntektsmelding;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import no.nav.foreldrepenger.oversikt.domene.Saksnummer;
+import no.nav.foreldrepenger.oversikt.tilgangskontroll.TilgangKontrollTjeneste;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@Path("/inntektsmeldinger")
+@ApplicationScoped
+public class InntektsmeldingRest {
+    private static final Logger LOG = LoggerFactory.getLogger(InntektsmeldingRest.class);
+
+    private InntektsmeldingTjeneste inntektsmeldingTjeneste;
+    private TilgangKontrollTjeneste tilgangkontroll;
+
+    @Inject
+    public InntektsmeldingRest(InntektsmeldingTjeneste inntektsmeldingTjeneste, TilgangKontrollTjeneste tilgangkontroll) {
+        this.inntektsmeldingTjeneste = inntektsmeldingTjeneste;
+        this.tilgangkontroll = tilgangkontroll;
+    }
+
+    InntektsmeldingRest() {
+        // CDI
+    }
+
+    @GET
+    @Path("/alle")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<InntektsmeldingDto> alleInntektsmeldinger(@QueryParam("saksnummer") @Valid @NotNull Saksnummer saksnummer) {
+        tilgangkontroll.sjekkAtKallErFraBorger();
+        tilgangkontroll.tilgangssjekkMyndighetsalder();
+        var inntektsmeldinger = inntektsmeldingTjeneste.inntektsmeldinger(saksnummer);
+        LOG.info("Hentet {} inntektsmeldinger på sak {}", inntektsmeldinger.size(), saksnummer.value());
+        return inntektsmeldinger;
+    }
+}
