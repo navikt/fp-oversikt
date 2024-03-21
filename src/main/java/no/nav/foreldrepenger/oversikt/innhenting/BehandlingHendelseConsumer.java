9376b331dfa60a47cfeb49c949122c24a2610d86
Anders Palfi <anderspalfi@gmail.com>
2024-03-21 13:16:11 +0100

    Går fra kafka streams til kafka comsumer (#236)

--- src/main/java/no/nav/foreldrepenger/oversikt/innhenting/BehandlingHendelseConsumer.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/innhenting/BehandlingHendelseConsumer.java
@@ -1,107 +1,51 @@
 package no.nav.foreldrepenger.oversikt.innhenting;
 
-import static org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_CLIENT;
+import java.util.List;
 
-import java.time.Duration;
-
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import no.nav.foreldrepenger.konfig.Environment;
-import no.nav.foreldrepenger.konfig.KonfigVerdi;
-import no.nav.vedtak.felles.integrasjon.kafka.KafkaProperties;
+import no.nav.vedtak.felles.integrasjon.kafka.KafkaConsumerManager;
+import no.nav.vedtak.log.metrics.Controllable;
+import no.nav.vedtak.log.metrics.LiveAndReadinessAware;
 
 @ApplicationScoped
-public class BehandlingHendelseConsumer {
+public class BehandlingHendelseConsumer implements LiveAndReadinessAware, Controllable {
 
     private static final Logger LOG = LoggerFactory.getLogger(BehandlingHendelseConsumer.class);
-    private static final Environment ENV = Environment.current();
 
-    private static final String PROD_APP_ID = "fpoversikt-behandling"; // Hold konstant pga offset commit !!
-    private static final int HANDLE_MESSAGE_INTERVAL_MILLIS = 20;
-
-    private String topicName;
-    private KafkaStreams stream;
+    private KafkaConsumerManager<String, String> kcm;
 
     public BehandlingHendelseConsumer() {
     }
 
     @Inject
-    public BehandlingHendelseConsumer(@KonfigVerdi(value = "kafka.behandlinghendelse.topic", defaultVerdi = "teamforeldrepenger.behandling-hendelse-v1") String topicName,
+    public BehandlingHendelseConsumer(BehandlingMigreringHåndterer behandlingMigreringHåndterer,
                                       BehandlingHendelseHåndterer behandlingHendelseHåndterer) {
-        this.topicName = topicName;
-
-        final Consumed<String, String> consumed = Consumed.with(Topology.AutoOffsetReset.EARLIEST);
-
-        final StreamsBuilder builder = new StreamsBuilder();
-        builder.stream(topicName, consumed)
-            .foreach((key, payload) -> {
-                behandlingHendelseHåndterer.handleMessage(topicName, key, payload);
-                sleep();
-            });
-
-        this.stream = new KafkaStreams(builder.build(), KafkaProperties.forStreamsStringValue(getApplicationId()));
+        kcm = new KafkaConsumerManager<>(List.of(behandlingHendelseHåndterer, behandlingMigreringHåndterer));
     }
 
-    private static void sleep() {
-        try {
-            Thread.sleep(HANDLE_MESSAGE_INTERVAL_MILLIS);
-        } catch (InterruptedException e) {
-            LOG.warn("Interrupt!", e);
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    public void start() {
-        addShutdownHooks();
-        stream.start();
-        LOG.info("Starter konsumering av topic={}, tilstand={}", getTopicName(), stream.state());
-    }
-
-    public void stop() {
-        LOG.info("Starter shutdown av topic={}, tilstand={} med 15 sekunder timeout", getTopicName(), stream.state());
-        stream.close(Duration.ofSeconds(15));
-        LOG.info("Shutdown av topic={}, tilstand={} med 15 sekunder timeout", getTopicName(), stream.state());
-    }
-
+    @Override
     public boolean isAlive() {
-        return (stream != null && stream.state().isRunningOrRebalancing());
+        return kcm.allRunning();
     }
 
+    @Override
     public boolean isReady() {
         return isAlive();
     }
 
-    private void addShutdownHooks() {
-        stream.setStateListener((newState, oldState) -> {
-            LOG.info("{} :: From state={} to state={}", getTopicName(), oldState, newState);
-
-            if (newState == KafkaStreams.State.ERROR) {
-                // if the stream has died there is no reason to keep spinning
-                LOG.warn("{} :: No reason to keep living, closing stream", getTopicName());
-                stop();
-            }
-        });
-        stream.setUncaughtExceptionHandler(ex -> {
-            LOG.error("{} :: Caught exception in stream, exiting", getTopicName(), ex);
-            return SHUTDOWN_CLIENT;
-        });
+    @Override
+    public void start() {
+        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
+        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
     }
 
-    private String getTopicName() {
-        return topicName;
-    }
-
-    private static String getApplicationId() {
-        if (!ENV.isProd()) {
-            return PROD_APP_ID + (ENV.isDev() ? "-dev" : "-vtp");
-        }
-        return PROD_APP_ID;
+    @Override
+    public void stop() {
+        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
+        kcm.stop();
     }
 }
--- src/main/java/no/nav/foreldrepenger/oversikt/innhenting/journalføringshendelse/JournalføringHendelseHåndterer.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/innhenting/journalføringshendelse/JournalføringHendelseHåndterer.java
@@ -1,21 +1,44 @@
 package no.nav.foreldrepenger.oversikt.innhenting.journalføringshendelse;
 
 
+import static io.confluent.kafka.serializers.KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG;
 import static no.nav.foreldrepenger.oversikt.innhenting.journalføringshendelse.HentDataFraJoarkForHåndteringTask.JOURNALPOST_ID;
 
+import java.util.Map;
+import java.util.function.Supplier;
+
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.foreldrepenger.konfig.KonfigVerdi;
+import no.nav.foreldrepenger.oversikt.innhenting.journalføringshendelse.test.VtpKafkaAvroDeserializer;
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord;
+import no.nav.vedtak.felles.integrasjon.kafka.KafkaMessageHandler;
+import no.nav.vedtak.felles.integrasjon.kafka.KafkaProperties;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
 @ApplicationScoped
 @ActivateRequestContext
 @Transactional
-public class JournalføringHendelseHåndterer {
+public class JournalføringHendelseHåndterer implements KafkaMessageHandler<String, JournalfoeringHendelseRecord> {
 
+    private static final String GROUP_ID = "fpoversikt-journalføring"; // Hold konstant pga offset commit !!
+    private static final String TEMA_FOR = "FOR";
+    private static final String MOTTAKSKANAL_ALTINN = "ALTINN";
+    private static final String MOTTAKSKANAL_SELVBETJENING = "NAV_NO";
+    private static final String HENDELSE_ENDELIG_JOURNALFØRT = "EndeligJournalført";
+
+    private static final Environment ENV = Environment.current();
+    private static final Map<String, Object> SCHEMA_MAP = getSchemaMap();
+
+    private String topicName;
     private ProsessTaskTjeneste taskTjeneste;
 
     JournalføringHendelseHåndterer() {
@@ -23,10 +46,28 @@ public class JournalføringHendelseHåndterer {
     }
 
     @Inject
-    public JournalføringHendelseHåndterer(ProsessTaskTjeneste taskTjeneste) {
+    public JournalføringHendelseHåndterer(@KonfigVerdi("kafka.topic.journal.hendelse") String topicName,
+                                          ProsessTaskTjeneste taskTjeneste) {
+        this.topicName = topicName;
         this.taskTjeneste = taskTjeneste;
     }
 
+    @Override
+    public void handleRecord(String key, JournalfoeringHendelseRecord value) {
+        if (hendelseSkalHåndteres(value)) {
+            handleMessage(value);
+        }
+
+    }
+
+    private static boolean hendelseSkalHåndteres(JournalfoeringHendelseRecord value) {
+        return TEMA_FOR.equals(value.getTemaNytt()) && (MOTTAKSKANAL_ALTINN.equals(value.getMottaksKanal())
+            || MOTTAKSKANAL_SELVBETJENING.equals(value.getMottaksKanal()) && HENDELSE_ENDELIG_JOURNALFØRT.equals(value.getHendelsesType()));
+    }
+
+    /*
+     * Dokumentasjon https://confluence.adeo.no/display/BOA/Joarkhendelser
+     */
     void handleMessage(JournalfoeringHendelseRecord payload) {
         var journalpostId = String.valueOf(payload.getJournalpostId());
         var kanalreferanse = payload.getKanalReferanseId();
@@ -44,5 +85,49 @@ public class JournalføringHendelseHåndterer {
 
     private static boolean harVerdi(String kanalreferanse) {
         return kanalreferanse != null && !kanalreferanse.isEmpty() && !kanalreferanse.isBlank();
+    }
+
+    @Override
+    public String topic() {
+        return topicName;
+    }
+
+    @Override
+    public String groupId() {
+        return GROUP_ID;
+    }
+
+    @Override
+    public Supplier<Deserializer<String>> keyDeserializer() {
+        return () -> {
+            var s = new StringDeserializer();
+            s.configure(SCHEMA_MAP, true);
+            return s;
+        };
+    }
+
+    @Override
+    public Supplier<Deserializer<JournalfoeringHendelseRecord>> valueDeserializer() {
+        return () -> {
+            var s = getDeserializer();
+            s.configure(SCHEMA_MAP, false);
+            return s;
+        };
+    }
+
+    private static Deserializer<JournalfoeringHendelseRecord> getDeserializer() {
+        return ENV.isProd() || ENV.isDev() ? new WrappedAvroDeserializer<>() : new WrappedAvroDeserializer<>(new VtpKafkaAvroDeserializer());
+    }
+
+    private static Map<String, Object> getSchemaMap() {
+        var schemaRegistryUrl = KafkaProperties.getAvroSchemaRegistryURL();
+        if (schemaRegistryUrl != null && !schemaRegistryUrl.isEmpty()) {
+            return Map.of(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl,
+                AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO",
+                AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG, KafkaProperties.getAvroSchemaRegistryBasicAuth(),
+                SPECIFIC_AVRO_READER_CONFIG, true);
+        } else {
+            return Map.of();
+        }
     }
 }
