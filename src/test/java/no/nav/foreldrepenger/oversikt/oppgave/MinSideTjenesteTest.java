14941370c607bb5136974cefce7277060c8b3a43
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-12-13 15:34:03 +0100

    Bump minside-varsler (#326)

---
+++ src/test/java/no/nav/foreldrepenger/oversikt/oppgave/MinSideTjenesteTest.java
@@ -0,0 +1,51 @@
+package no.nav.foreldrepenger.oversikt.oppgave;
+
+import no.nav.foreldrepenger.oversikt.domene.AktørId;
+import no.nav.foreldrepenger.oversikt.domene.YtelseType;
+import no.nav.foreldrepenger.oversikt.stub.DummyPersonOppslagSystemTest;
+
+import no.nav.tms.varsel.builder.BuilderEnvironment;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.net.MalformedURLException;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+
+class MinSideTjenesteTest {
+
+    private MinSideProducer producer;
+    private MinSideTjeneste tjeneste;
+
+    @BeforeEach
+    void setup() throws MalformedURLException {
+        var pdl = new DummyPersonOppslagSystemTest(null, null);
+        producer = mock(MinSideProducer.class);
+        tjeneste = new MinSideTjeneste(pdl, null, producer, "https://www.nav.no/foreldrepenger/oversikt");
+        BuilderEnvironment.extend(Map.of(
+            "NAIS_CLUSTER_NAME", "test",
+            "NAIS_NAMESPACE", "test",
+            "NAIS_APP_NAME", "test"
+        ));
+    }
+
+    @Test
+    void skal_sende_beskjed_om_mottatt_søknad() {
+        var uuidArgument = ArgumentCaptor.forClass(UUID.class);
+        var jsonArgument = ArgumentCaptor.forClass(String.class);
+        var aktørId = new AktørId("12345678901");
+        tjeneste.sendBeskjedVedInnkommetSøknad(aktørId, YtelseType.FORELDREPENGER, true, UUID.randomUUID());
+
+        verify(producer).send(uuidArgument.capture(), jsonArgument.capture());
+
+        assertThat(jsonArgument.getValue()).contains("Vi mottok en søknad om endring av foreldrepenger");
+    }
+
+}
