98227c461a8f1846de27ac5c87632e2f1fa6445f
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-03-31 09:04:58 +0200

    tilgang: Bedre caching av ident, alder, mv (#353)

--- src/main/java/no/nav/foreldrepenger/oversikt/saker/PdlKlient.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/saker/PdlKlient.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.oversikt.saker;
 
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import no.nav.pdl.PersonResponseProjection;
 import no.nav.vedtak.felles.integrasjon.person.AbstractPersonKlient;
 import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
 import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
+import no.nav.vedtak.util.LRUCache;
 
 @RestClientConfig(
     tokenConfig = TokenFlow.ADAPTIVE,
@@ -25,17 +27,25 @@ class PdlKlient extends AbstractPersonKlient {
 
     private static final Logger LOG = LoggerFactory.getLogger(PdlKlient.class);
 
+    private static final long CACHE_ELEMENT_LIVE_TIME_MS = TimeUnit.MILLISECONDS.convert(60, TimeUnit.MINUTES);
+    private static final LRUCache<String, LocalDate> FNR_FØDT = new LRUCache<>(1000, CACHE_ELEMENT_LIVE_TIME_MS);
+
     LocalDate fødselsdato(String fnr) {
         LOG.debug("Henter fødselsdato");
+        if (FNR_FØDT.get(fnr) != null) {
+            return FNR_FØDT.get(fnr);
+        }
         var request = new HentPersonQueryRequest();
         request.setIdent(fnr);
         var projection = new PersonResponseProjection()
             .foedselsdato(new FoedselsdatoResponseProjection().foedselsdato());
         var person = hentPerson(request, projection);
-        return person.getFoedselsdato().stream()
+        var fødselsdato = person.getFoedselsdato().stream()
             .findFirst()
             .map(Foedselsdato::getFoedselsdato)
             .map(LocalDate::parse)
             .orElseThrow();
+        FNR_FØDT.put(fnr, fødselsdato);
+        return fødselsdato;
     }
 }
