f49b2cd99d9c1869cfdc334e9f92da4af00d16dc
espenwaaga <espen.waaga@nav.no>
2025-11-05 13:40:15 +0100

    Bump fp-kontrakt til 9.4.8

--- src/main/java/no/nav/foreldrepenger/oversikt/domene/fp/Gradering.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/domene/fp/Gradering.java
@@ -1,12 +1,14 @@
 package no.nav.foreldrepenger.oversikt.domene.fp;
 
 import no.nav.foreldrepenger.kontrakter.fpoversikt.Aktivitet;
+import no.nav.foreldrepenger.kontrakter.fpoversikt.Arbeidstidprosent;
 import no.nav.foreldrepenger.oversikt.domene.Prosent;
 
 public record Gradering(Prosent prosent, UttakAktivitet uttakAktivitet) {
     public no.nav.foreldrepenger.kontrakter.fpoversikt.Gradering tilDto() {
         var arbeidsgiver = uttakAktivitet().arbeidsgiver();
-        return new no.nav.foreldrepenger.kontrakter.fpoversikt.Gradering(prosent().decimalValue(),
+        return new no.nav.foreldrepenger.kontrakter.fpoversikt.Gradering(
+            new Arbeidstidprosent(prosent().decimalValue()),
             new Aktivitet(uttakAktivitet().type().tilDto(), arbeidsgiver == null ? null : arbeidsgiver.tilDto(), null));
     }
 }
