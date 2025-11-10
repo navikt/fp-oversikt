343f49d5724e65a432d068d63409b8a7246dc180
espenwaaga <espen.waaga@nav.no>
2025-11-10 12:03:46 +0100

    Fix: ForvaltningApiConfig manglet ApplicationPath

--- src/main/java/no/nav/foreldrepenger/oversikt/server/konfig/ForvaltningApiConfig.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/server/konfig/ForvaltningApiConfig.java
@@ -8,11 +8,13 @@ import java.util.Set;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import jakarta.ws.rs.ApplicationPath;
 import no.nav.foreldrepenger.oversikt.drift.ManuellOppdateringAvSakDriftTjeneste;
 import no.nav.foreldrepenger.oversikt.server.konfig.swagger.OpenApiUtils;
 import no.nav.foreldrepenger.oversikt.server.sikkerhet.ForvaltningAuthorizationFilter;
 import no.nav.vedtak.felles.prosesstask.rest.ProsessTaskRestTjeneste;
 
+@ApplicationPath(ForvaltningApiConfig.API_URI)
 public class ForvaltningApiConfig extends ResourceConfig {
     public static final String API_URI = "/forvaltning/api";
 
