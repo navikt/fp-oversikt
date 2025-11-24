23e72e1472035163dcccc977cfbb4339ccdab6c2
johannbm <johannes.moskvil@gmail.com>
2025-11-24 11:55:14 +0100

    ny dto

--- src/main/java/no/nav/foreldrepenger/oversikt/domene/beregning/BeregningV1.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/domene/beregning/BeregningV1.java
@@ -1,6 +1,23 @@
 package no.nav.foreldrepenger.oversikt.domene.beregning;
 
-import no.nav.foreldrepenger.oversikt.domene.Beløp;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
-public record BeregningV1(Beløp dagsats) implements Beregning {
+public record BeregningV1(LocalDate skjæringsTidspunkt, List<BeregningsAndel> beregningsAndeler, List<BeregningAktivitetStatus> beregningAktivitetStatuser) implements Beregning {
+
+    // TODO: bruke denne AktivtetStatus?
+    public record BeregningsAndel(Object aktivitetStatus, BigDecimal fastsattPrMnd, InntektsKilde inntektsKilde, Arbeidsforhold arbeidsforhold, BigDecimal dagsats) {}
+
+    public record Arbeidsforhold(String arbeidsgiverIdent, BigDecimal refusjonPrMnd) {}
+
+    public record BeregningAktivitetStatus(Object aktivitetStatus, Object hjemmel) {}
+
+    public enum InntektsKilde {
+        INNTEKTSMELDING,
+        A_INNTEKT,
+        VEDTAK_ANNEN_YTELSE,
+        SKJØNNSFASTSATT,
+        PGI // Pensjonsgivendeinntekt
+    }
 }
--- src/main/java/no/nav/foreldrepenger/oversikt/innhenting/beregning/FpSakBeregningDto.java
+++ src/main/java/no/nav/foreldrepenger/oversikt/innhenting/beregning/FpSakBeregningDto.java
@@ -2,5 +2,24 @@ package no.nav.foreldrepenger.oversikt.innhenting.beregning;
 
 import no.nav.foreldrepenger.oversikt.domene.Beløp;
 
-public record FpSakBeregningDto(Beløp dagsats) {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public record FpSakBeregningDto(LocalDate skjæringsTidspunkt, List<BeregningsAndel> beregningsAndeler, List<BeregningAktivitetStatus> beregningAktivitetStatuser) {
+
+    // TODO: bruke denne AktivtetStatus?
+    public record BeregningsAndel(Object aktivitetStatus, BigDecimal fastsattPrMnd, InntektsKilde inntektsKilde, Arbeidsforhold arbeidsforhold, BigDecimal dagsats) {}
+
+    public record Arbeidsforhold(String arbeidsgiverIdent, BigDecimal refusjonPrMnd) {}
+
+    public record BeregningAktivitetStatus(Object aktivitetStatus, Object hjemmel) {}
+
+    public enum InntektsKilde {
+        INNTEKTSMELDING,
+        A_INNTEKT,
+        VEDTAK_ANNEN_YTELSE,
+        SKJØNNSFASTSATT,
+        PGI // Pensjonsgivendeinntekt
+    }
 }
